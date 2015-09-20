059b56a1ea55b523bfc414f1fff49babe8b3cd23
san-na <wslman77@gmail.com>
2015-09-19 21:43:08 -0400

    为浏览器添加书签列表

--- Python/graphics/web_browser.py
+++ Python/graphics/web_browser.py
@@ -32,12 +32,21 @@ class Browser(QWidget):
         self.go_btn.clicked.connect(self.go_btn_clicked)
         self.go_btn.setMaximumSize(20, 20)
 
+        self.favourites = QComboBox(self)
+        self.favourites.addItems(['http://www.baidu.com',
+                            'http://www.qq.com',
+                            'http://www.cnblogs.com'])
+        self.favourites.activated.connect(self.favourite_selected)
+        self.favourites.setMinimumSize(200, 20)
+        self.favourites.setMaximumSize(300, 20)
+
         self.menu_bar = QHBoxLayout()
         self.menu_bar.addWidget(self.back_btn)
         self.menu_bar.addWidget(self.forward_btn)
         self.menu_bar.addWidget(self.url_entry)
         self.menu_bar.addWidget(self.go_btn)
         self.menu_bar.addStretch()
+        self.menu_bar.addWidget(self.favourites)
         self.main_layout = QVBoxLayout()
         self.main_layout.addLayout(self.menu_bar)
         self.main_layout.addWidget(self.webview)
@@ -45,6 +54,9 @@ class Browser(QWidget):
 
     def go_btn_clicked(self):
         self.webview.load(self.url_entry.text())
+
+    def favourite_selected(self):
+        self.webview.load(self.favourites.currentText())
 
 
 class BrowserWindow(QMainWindow):
